27273d3b43ee26e18b04dd4c64edaa12dd55d35e
situodys <situodys@gmail.com>
2022-07-22 23:02:15 +0900

    Feat: boardService 추가

--- src/main/java/com/example/board/dto/BoardDTO.java
+++ src/main/java/com/example/board/dto/BoardDTO.java
@@ -1,4 +1,23 @@
 package com.example.board.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDTO {
+
+    private Long bno;
+
+    private String title;
+    private String content;
+    private String writerEmail;
+    private String writerName;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+    private int replyCnt;
 }

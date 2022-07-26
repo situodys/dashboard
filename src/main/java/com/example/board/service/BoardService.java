fa93be554636b3cee73e86a4929e8b74fcc65749
situodys <situodys@gmail.com>
2022-07-26 13:59:02 +0900

    Feat: crud service api 생성

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -12,4 +12,6 @@ public interface BoardService {
     BoardDTO get(Long bno);
 
     void removeWithReplies(Long bno);
+
+    void modify(BoardDTO boardDTO);
 }
--- src/main/java/com/example/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -64,5 +65,17 @@ public class BoardServiceImpl implements BoardService {
     public void removeWithReplies(Long bno) {
         replyRepository.deleteByBno(bno);
         boardRepository.deleteById(bno);
+    }
+
+    @Override
+    public void modify(BoardDTO boardDTO) {
+        log.info(boardDTO);
+
+        Board board = boardRepository.getReferenceById(boardDTO.getBno());
+
+        board.changeTitle(boardDTO.getTitle());
+        board.changeContent(boardDTO.getContent());
+
+        boardRepository.save(board);
     }
 }
